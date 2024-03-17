62789d4547285e90e0d1b8427e6416482e249cb2
hymmns <esholadoye@gmail.com>
2024-03-17 03:52:16 +0100

    fix code output error

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 
-from, to, flags =  ARGV[0].scan(/(from|to|flags):(.*?)\]/).transpose
+from, to, flags =  ARGV[0].scan(/(from|to|flags):([^\]]*)/).transpose.map(&:compact)
 puts [from, to, flags].join(',')
